51f7d73d5cc512ecfba0bcdd06083c4f1b2a5776
Dhruv Desai <dhruvdesai770@gmail.com>
2024-08-06 09:21:26 -0400

    Changes made to LibraryService

--- src/services/LibraryService.java
+++ src/services/LibraryService.java
@@ -7,8 +7,13 @@ import java.util.List;
 
 public class LibraryService {
     List<Book> booklist = new LinkedList<Book>();
-    public List<Book> addBook(Book book){
+    public List<Book> addBook(Book book) {
         booklist.add(book);
         return booklist;
+
+    }
+        public List<Book> removeBook(Book book){
+            booklist.add(book);
+            return booklist;
     }
 }
